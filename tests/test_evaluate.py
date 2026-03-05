8af23e992124bcce252afca04d21c65c3e443991
Diego <diegoalfaromtv@gmail.com>
2026-03-05 18:33:32 +0100

    added all tests for the different src modules

---
+++ tests/test_evaluate.py
@@ -0,0 +1,9 @@
+"""Tests for src.evaluate module."""
+
+
+class TestEvaluate:
+    """Placeholder tests for the evaluate module."""
+
+    def test_placeholder(self):
+        """Remove once real evaluation functions are implemented."""
+        assert True
---
+++ tests/test_train.py
@@ -0,0 +1,9 @@
+"""Tests for src.train module."""
+
+
+class TestTrain:
+    """Placeholder tests for the train module."""
+
+    def test_placeholder(self):
+        """Remove once real training functions are implemented."""
+        assert True
